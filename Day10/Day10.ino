d0bced3a3cce1f779cd55f200a8dbc5bcc368898
Doc Malone <michaelpmalone24@gmail.com>
2026-05-22 20:30:26 -0600

    Review to get back up to speed

--- Day10/Day10.ino
+++ Day10/Day10.ino
@@ -1,122 +1,72 @@
-/*
- * 30 Days - Lost in Space
- * Day 11 - Starting your control panel
- *
- * Learn more at https://learn.inventr.io/adventure
- *
- * Today we introduce you to a 4x4 button Keypad.  This keypad is very similar to
- * what is found on a calculator or even a digital phone.  This keypad uses only 8
- * pins to encode 16 buttons by dividing the keypad into rows and columns.  When
- * a button is pressed it can be identified by finding which row pin and which column
- * pin are pressed.
- *
- * We simplify this process by using a library written for decoding similar keypads.
- *
- * Alex Eschenauer
- * David Schmidt
- * Greg Lyzenga
- */
- 
-/*
- * Arduino concepts introduced/documented in this lesson.
- * - libraries: Code provided by others that we can use in our sketches
- * - char type: Represents a single character like 'A', 'd', '4' or '*'.
- * - arrays:    Variables of the same type arranged as a list where each item
- *              can be accessed using an index.  Arrays can be one dimensional
- *              like a list (with one index) or two dimensional like a spreadsheet
- *              table with rows and columns (using row and column indexes).
- *
- * Parts and electronics concepts introduced in this lesson.
- */
- 
 // Explicitly include Arduino.h
 #include "Arduino.h"
  
+// Our photoresistor will give us a reading of the current light level on this analog pin
+const byte PHOTORESISTOR_PIN = A0;  // Photoresistor analog pin
+ 
+// RGB LED pins
+const byte RED_PIN = 11;    // pin controlling the red leg of our RGB LED
+const byte GREEN_PIN = 10;  // pin ccontrolling the green leg of our RGB LED
+const byte BLUE_PIN = 9;    // pin ccontrolling the blue leg of our RGB LED
+
+// Switch input pins
+const byte ALWAYS_ON = 2;   // pin for the switch that would indicate the light should be always on
+const byte ALWAYS_OFF = 3;  // pin for the switch that would indicate the light should be always off
+ 
+const unsigned long BATTERY_CAPACITY = 50000;  // Maximum battery capacity
+ 
 /*
- * Instead of writing all the code necessary to check all 8 pins and calculate
- * which button is pressed we will use a "library" that has already been written
- * for us and simply call functions in that library.
- *
- * Please refer to the lesson and lesson videos to load the Keypad library into
- * your Arduino IDE before you use it for the first time.
- *
- * In order to use the Keypad library we include the file "Keypad.h".
+ * Display a color on our RGB LED by providing an intensity for
+ * our red, green and blue LEDs.
  */
-#include <Keypad.h>
- 
-// Our keypad has 4 rows, each with 4 columns.
-const byte ROWS = 4;
-const byte COLS = 4;
- 
-/*
- * A one dimensional array is like a list of similar variables.  We use an "index" number
- * to access each item.  To access values in an array we use the name of the array followed
- * immediately by the index in square brackets: name[index].  The index can either be a
- * constant ("name[3]") or a variable containing a value:
- *
- * byte my_index = 3;
- * name[my_index] = value;
- *
- * Indexes start with zero, not one.  Think of the index as a number *added* to the first
- * location.  So, to access the first item in our array we add 0.  To access the second
- * item in the array we add 1.
- *
- * In our first array below, ROW_PINS[0] is set to 5 and ROW_PINS[2] is 3.
- */
- 
-// These constant arrays contain the pins connected to the keypad's row and column pins.
-const byte ROW_PINS[ROWS] = { 5, 4, 3, 2 };
-const byte COL_PINS[COLS] = { 6, 7, 8, 9 };
- 
-/*
-  * Character Type (char)
-  *
-  * In Arduino C++ we have previousluy used strings of characters using double quotes ("my
-  * string").  But for a single character we need to use single quotes to indicate that there
-  * is just ONE character ('A').
-  */
- 
-/*
- * Two dimensional array
- *
- * A two dimensional array combines multiple rows of one-dimensional array into a table
- * using two indexes using square brackets.  To access any item in the array the first
- * index is added to the row, and then the second index is added to the column.
- *
- * In the BUTTONS array below BUTTONS[0][0] is set to the character '1' and BUTTONS[2][3]
- * is set to the character 'C'.
- */
- 
-// Since the keypad has 4 rows of 4 columns each we use a two dimensional array to
-// configure what character will be returned when each button is pressed.
-const char BUTTONS[ROWS][COLS] = {
-  { '1', '2', '3', 'A' },  // Row 0
-  { '4', '5', '6', 'B' },  // Row 1
-  { '7', '8', '9', 'C' },  // Row 2
-  { '*', '0', '#', 'D' }   // Row 3
-};
- 
-/*
- * Using the Keypad.h Library, we are going to define a custom keypad to return
- * the characters we wish when each key is pressed.
- *
- * To do this, we create a heroKeypad object configured with our BUTTONS array,
- * the pins used for the rows and columns and lastly how many rows and how many
- * columns are on our kepad.
- *
- * NOTE: this also does the proper pinMode() commands so we don't need them in our setup()
- */
-Keypad heroKeypad = Keypad(makeKeymap(BUTTONS), ROW_PINS, COL_PINS, ROWS, COLS);
+void displayColor(
+  byte red_intensity,    // red LED intensity (0-255)
+  byte green_intensity,  // green LED intensity (0-255)
+  byte blue_intensity    // blue LED intensity (0-255)
+) {
+  analogWrite(RED_PIN, red_intensity);      // write red LED intensity using PWM
+  analogWrite(GREEN_PIN, green_intensity);  // write green LED intensity using PWM
+  analogWrite(BLUE_PIN, blue_intensity);    // write blue LED intensity using PWM
+}
  
 void setup() {
-  Serial.begin(9600);  // Initialize the serial monitor
+  // Declare the RGB LED pins as outputs:
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+
+  // Declare the switch pins as inputs
+  pinMode(ALWAYS_ON, INPUT);
+  pinMode(ALWAYS_OFF, INPUT);
+ 
+  // Start serial monitor
+  Serial.begin(9600);
 }
  
 void loop() {
-  // heroKeypad.waitForKey() will wait here until any button is pressed and
-  // returns the character we defined in our BUTTONS array.
-  char pressedButton = heroKeypad.waitForKey();  // Wait until a button is pressed
+  static unsigned long photo_in = 0;
+   photo_in = analogRead(PHOTORESISTOR_PIN);  // Add current "charge amount" to our battery
+  
+  if (digitalRead(ALWAYS_ON) && !digitalRead(ALWAYS_OFF)){
+    displayColor(128, 128, 128);  //On switch is on, off switch is off, turn light on
+  } else if (digitalRead(ALWAYS_OFF) && !digitalRead(ALWAYS_ON)) {
+    displayColor(0, 0, 0);  //Off switch is on, on switch is off, turn light off
+  } else {
+    if (photo_in <= 5) {
+      displayColor(128, 128, 128);
+    } else if (photo_in > 5 && photo_in <= 25) {
+      displayColor(32, 32, 32);
+    } else {
+      displayColor(0, 0, 0);
+    }
+  }
  
-  // Display the character returned for the button that was pressed.
-  Serial.println(pressedButton);
+  Serial.print("On Switch: ");  
+  Serial.print(digitalRead(ALWAYS_ON));
+  Serial.print(", Off Switch: ");
+  Serial.print(digitalRead(ALWAYS_OFF));
+  Serial.print(", Photo level: ");
+  Serial.println(analogRead(PHOTORESISTOR_PIN));
+ 
+  delay(100);  // Delay 1/10 of a second so displayed values don't scroll too fast
 }
